7702218d995d278770a30197ccfcf47b4933d87c
Alexkeis758 <54685555+Alexkeis758@users.noreply.github.com>
2019-09-03 18:00:14 -0600

    Week 1 Activity

---
+++ AccountsActivity/AccountsActivity_KeishaAlexander.py
@@ -0,0 +1,8 @@
+def test_activity_zero():
+    moviequote = 'Doth mother know you weareth her drapes?'
+    print(moviequote)
+    assert False
+
+    
+def test_pull_request_add():
+    assert True
